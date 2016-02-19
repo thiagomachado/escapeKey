748a63b9bc7fa7720d6347b5397990605e56b47a
patykov <patriciakovaleski@gmail.com>
2016-02-19 14:53:52 -0200

    Agora add um inimigo estatico

--- js/model/Enemy.js
+++ js/model/Enemy.js
@@ -1,4 +1,4 @@
-var Player = function(game, scene, gamepad)
+var Enemy = function(game, scene, gamepad)
 {
 
     this.game = game;
@@ -10,7 +10,7 @@ var Player = function(game, scene, gamepad)
     this.box.position.y = 2;
     this.body = this.box.setPhysicsState(BABYLON.PhysicsEngine.BoxImpostor, {mass:1, friction:0.1, restitution:0.1});
     var matos = new BABYLON.StandardMaterial("matos", this.scene);
-    matos.diffuseColor = BABYLON.Color3.Green();
+    matos.diffuseColor = BABYLON.Color3.Red();
     this.box.material = matos;
 
     // The player speed
@@ -18,7 +18,7 @@ var Player = function(game, scene, gamepad)
 
     // The player vision range
     this.visionRange = BABYLON.Mesh.CreateSphere("vision", 10, this.scene);
-    this.visionRange.position.y = enemy.position;
+    this.visionRange.position.y = this.position;
 
     /* GAMEPAD*/
     var _this = this;
@@ -44,18 +44,18 @@ Enemy.prototype =
         this.mvtDirection[direction] = value;
     },
 
-    vision : function(){
+    /*vision : function(){
         p_pos = game.Player.mvtDirection;
-        
+
         if ()
 
-    },
+    },*/
 
     move : function()
     {
 
         var s = 5;
-
+/*
         if (this.mvtDirection[0] != 0) {
             this.box.applyImpulse(new BABYLON.Vector3(0,0,s), this.box.position);
         }
@@ -70,6 +70,7 @@ Enemy.prototype =
         }
         this.body.body.linearVelocity.scaleEqual(0.92);
         this.body.body.angularVelocity.scaleEqual(0);
+        */
     },
 
 
